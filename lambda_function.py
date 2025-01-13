c8521c9ccb0720b6c124c15f0088d8b4c8a3b0c2
Vishal <vishal@Balas-MacBook-Pro.local>
2025-01-13 21:53:43 +0530

    Basic functionality

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,8 @@ sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='AlexaSmarthome.fifo')
 
 sresobj = SmartHomeResponseHandler()
-sresobj.addThermostat(manufacturer="LG",name="AC",description="AC",device_id="endpoint-lg-ac",targetSetpoint=True,thermostatMode=True,supportedModes=["COOL","OFF"],power=True,temperatureRetrievable=False,powerRetrievable=False)
+sresobj.addLightAppliance(manufacturer="iHome",name="Bedroom Light",description="Light",device_id="endpoint-ihome-light")
+sresobj.addSwitchAppliance(manufacturer="iHome",name="Bedroom Fan",description="Fan",device_id="endpoint-ihome-fan")
 
 def form_message(device, operation, extravalue="") :
     timestamp = str(uuid.uuid4())
@@ -20,19 +21,25 @@ def form_message(device, operation, extravalue="") :
 
 def lambda_handler(event, context):
     sreqobj = SmartHomeRequestHandler(event)
+    otoken = sreqobj.get_authentication_details()["token"]
     print(sreqobj.get_request_name())
     if sreqobj.get_request_name() == "Discover":
         return sresobj.buildDiscoveryResponse()
-    elif sreqobj.get_request_name() == "SetThermostatMode":
-        if sreqobj.get_thermostat_mode() == "COOL":
-            print("turn on ac")
-            queue.send_message(MessageBody=form_message("AC", 'on'),MessageGroupId='messageGroup1')
-            return sresobj.buildTemperatureResponse(endpoint="endpoint-lg-ac",powerState="ON",targetSetpointValue=26,mode="COOL")
-        else:
-            print("turn off ac")
-            queue.send_message(MessageBody=form_message("AC", 'off'),MessageGroupId='messageGroup1')
-            return sresobj.buildTemperatureResponse(endpoint="endpoint-lg-ac",powerState="OFF")
-    elif sreqobj.get_request_name() == "SetTargetTemperature":
-        print("turn off ac")
-        queue.send_message(MessageBody=form_message("AC", 'temperature', float(sreqobj.get_thermostat_temperature()["targetSetpoint"]["value"])),MessageGroupId='messageGroup1')
-        return sresobj.buildTemperatureResponse(endpoint="endpoint-lg-ac",powerState="ON",targetSetpointValue=float(sreqobj.get_thermostat_temperature()["targetSetpoint"]["value"]),mode="COOL")
+    elif sreqobj.get_request_name() == "TurnOn":
+        if sreqobj.get_endpoint_id() == "endpoint-ihome-light":
+            print("turn on light")
+            queue.send_message(MessageBody=form_message("LIGHT", 'on'),MessageGroupId='messageGroup1')
+            return sresobj.buildSwitchResponse(endpoint="endpoint-ihome-light",powerState="ON",oauthToken=otoken)
+        elif sreqobj.get_endpoint_id() == "endpoint-ihome-fan":
+            print("turn on fan")
+            queue.send_message(MessageBody=form_message("FAN", 'on'),MessageGroupId='messageGroup1')
+            return sresobj.buildSwitchResponse(endpoint="endpoint-ihome-fan",powerState="ON",oauthToken=otoken)
+    elif sreqobj.get_request_name() == "TurnOff":
+        if sreqobj.get_endpoint_id() == "endpoint-ihome-light":
+            print("turn off light")
+            queue.send_message(MessageBody=form_message("LIGHT", 'off'),MessageGroupId='messageGroup1')
+            return sresobj.buildSwitchResponse(endpoint="endpoint-ihome-light",powerState="OFF",oauthToken=otoken)
+        elif sreqobj.get_endpoint_id() == "endpoint-ihome-fan":
+            print("turn on fan")
+            queue.send_message(MessageBody=form_message("FAN", 'off'),MessageGroupId='messageGroup1')
+            return sresobj.buildSwitchResponse(endpoint="endpoint-ihome-fan",powerState="OFF",oauthToken=otoken)

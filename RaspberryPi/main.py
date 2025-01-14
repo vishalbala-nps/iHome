3dd8cd1e50af77e67e66bb5731a8f8c4a825e9df
Vishal <vishal@Balas-MacBook-Pro.local>
2025-01-14 11:10:39 +0530

    GPIO Stuff

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -2,18 +2,31 @@ import boto3
 from dotenv import load_dotenv
 import os
 import json
+import gpiozero
 
 load_dotenv()
 
 client = boto3.client('sqs', region_name='eu-west-1', aws_access_key_id=os.getenv("AWS_ACCESS_ID"), aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"))
 resp = client.get_queue_url(QueueName="iHome.fifo")
 qURL=resp['QueueUrl']
+light = gpiozero.LED(21)
+fan = gpiozero.LED(20)
 
 def handleResponse(d):
     try:
         device = d["device"]
         op = d["operation"]
         print("Device: "+device+" Operation: "+op)
+        if device == "LIGHT":
+            if op == "on":
+                light.on()
+            elif op == "off":
+                light.off()
+        if device == "FAN":
+            if op == "on":
+                fan.on()
+            elif op == "off":
+                fan.off()
     except:
         print(d)
 
